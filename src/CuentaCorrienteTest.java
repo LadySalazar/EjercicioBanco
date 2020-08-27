52f0b5a2fe546281c376703e64575344fab2a468
LadySalazar <lady.salazar01@correo.usa.edu.co>
2020-08-27 09:05:42 -0500

    test Cuenta Corriente

--- src/CuentaCorrienteTest.java
+++ src/CuentaCorrienteTest.java
@@ -3,10 +3,7 @@ import org.junit.jupiter.api.Test;
 public class CuentaCorrienteTest {
     @Test
     public void determinarTasaInteres(){
-        TarjetaDebito tarDe=new TarjetaDebito(1014, 98,800000);
-        CuentaCorriente cuenta=new CuentaCorriente(1000,tarDe,800000);
-        double  d=cuenta.DeterminarTasaInteres();
-        assertEquals(1,d,0);
+
     }
 
 }
