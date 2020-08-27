3a1cc8fa2f8e91723c370553be33f4e64822ea79
Jessica <jessica.parrado01@correo.usa.edu.co>
2020-08-27 09:16:20 -0500

    Arreglo Test Corriente

--- src/CuentaCorrienteTest.java
+++ src/CuentaCorrienteTest.java
@@ -3,7 +3,10 @@ import org.junit.jupiter.api.Test;
 public class CuentaCorrienteTest {
     @Test
     public void determinarTasaInteres(){
-
+	TarjetaDebito tarextra=new TarjetaDebito(1209876, "02/02/2021",597,800000);
+        CuentaCorriente cuenta=new CuentaCorriente(4567,tarextra,800000);
+        double  d=cuenta.determinarTasaInteres();
+        assertEquals(1,d,0);
     }
 
 }
